d3af7eeeb54d42c09d552cb5203c53684d2cc937
Paul  Baker <paul.baker@wgu.edu>
2019-01-07 09:12:45 -0700

    Initial commit. Fairly decent bot for first mock up

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paul-nelson-baker/go-cookie-clicker
+
+require github.com/chromedp/chromedp v0.1.2
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"github.com/chromedp/cdproto/cdp"
+	"time"
+)
+import "github.com/chromedp/chromedp"
+import "log"
+
+const (
+	url = "http://orteil.dashnet.org/cookieclicker/"
+)
+
+var (
+	closeNotificationIfPresent = chromedp.ActionFunc(func(cc context.Context, ee cdp.Executor) error {
+		closeButtons := []*cdp.Node{}
+		chromedp.Nodes("#notes div.close", &closeButtons).Do(cc, ee)
+		if len(closeButtons) > 0 {
+			currentCloseButton := closeButtons[0]
+			chromedp.MouseClickNode(currentCloseButton).Do(cc, ee)
+		}
+		return nil
+	})
+	purchaseUpgradeIfAvailable = chromedp.ActionFunc(func(cc context.Context, ee cdp.Executor) error {
+		products := []*cdp.Node{}
+		chromedp.Nodes("div.upgrade.enabled", &products).Do(cc, ee)
+		if len(products) > 0 {
+			currentProduct := products[0]
+			chromedp.MouseClickNode(currentProduct).Do(cc, ee)
+		}
+		return nil
+	})
+	purchaseProductIfAvailable = chromedp.ActionFunc(func(cc context.Context, ee cdp.Executor) error {
+		products := []*cdp.Node{}
+		chromedp.Nodes("div.product.enabled", &products).Do(cc, ee)
+		if len(products) > 0 {
+			currentProduct := products[0]
+			chromedp.MouseClickNode(currentProduct).Do(cc, ee)
+		}
+		return nil
+	})
+)
+
+func main() {
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Do the bot that you do so well
+	err = c.Run(ctxt, chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.Click("a.cc_btn_accept_all"),
+	})
+	clickBigCookie := chromedp.Click("#bigCookie")
+	individualTasks := []chromedp.ActionFunc{purchaseUpgradeIfAvailable, purchaseProductIfAvailable, closeNotificationIfPresent}
+	for true {
+		c.Run(ctxt, chromedp.Tasks{
+			clickBigCookie,
+		})
+		for _, actionFunction := range individualTasks {
+			timeoutableContext, cancelFunc := context.WithTimeout(ctxt, 8*time.Millisecond)
+			c.Run(timeoutableContext, chromedp.Tasks{
+				actionFunction,
+			})
+			cancelFunc()
+		}
+	}
+	// Close all the stuff
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	err = c.Wait()
+	if err != nil {
+		log.Printf("%v", err)
+	}
+}
